153013e2b381aa94b9e0f13a4bfff3d596640440
Jonathan-Rios <jonathan.riosousa@gmail.com>
2020-12-02 07:21:40 -0300

    chore: Initial Project

---
+++ src/@types/index.d.ts
@@ -0,0 +1,2 @@
+declare module '*.png'; // Realizando isso, ele passa a aceitar importação
+// como se fosse arquivos JS, TS etc.
---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+import AuthRoutes from './auth.routes';
+import AppRoutes from './app.routes';
+
+import { useAuth } from '../hooks/auth';
+
+const Routes: React.FC = () => {
+  const { user, loading } = useAuth();
+
+  console.log('-----1', user);
+  console.log('-----2', loading);
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#666" />
+      </View>
+    );
+  }
+
+  return user ? <AppRoutes /> : <AuthRoutes />;
+};
+
+export default Routes;
